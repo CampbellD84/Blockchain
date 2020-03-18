b0039a53374a4b591d6f5356604ebab319c261be
Douglas Campbell <dacampbelltbone@gmail.com>
2020-03-18 00:49:39 -0400

    Fixed Blockchain in client_mining and completed miner.py. Only get a missing required fields message back.

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,14 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    str_block = json.dumps(block, sort_keys=True)
+
+    proof = 0
+
+    while valid_proof(str_block, proof) is False:
+        proof += 1
+
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,8 +34,13 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    hash_guess = hashlib.sha256(guess).hexdigest()
 
+    return hash_guess[:6] == '000000'
+
+
+coin_mined = 0
 
 if __name__ == '__main__':
     # What is the server address? IE `python3 miner.py https://server.com/api/`
@@ -46,17 +58,18 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
-        # Handle non-json response
-        try:
-            data = r.json()
-        except ValueError:
-            print("Error:  Non-json response")
-            print("Response returned:")
-            print(r)
-            break
+        # # Handle non-json response
+        # try:
+        data = r.json()
+        # except ValueError:
+        #     print("Error:  Non-json response")
+        #     print("Response returned:")
+        #     print(r)
+        #     break
 
-        # TODO: Get the block from `data` and use it to look for a new proof
+        # Get the block from `data` and use it to look for a new proof
         # new_proof = ???
+        new_proof = proof_of_work(data['last_block'])
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -67,4 +80,8 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data['message'] == 'Great Success! Forged a new proof.':
+            coin_mined += 1
+            print(coin_mined)
+        else:
+            print(data['message'])
